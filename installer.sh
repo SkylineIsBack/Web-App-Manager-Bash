7672a5e05a817479a22b285d3a8724cc2624fddd
SkylineIsBack <lmaoidkgit@gmail.com>
2021-09-28 01:32:43 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,17 +6,11 @@ echo "Installing zenity"
 sudo apt install zenity
 echo "Installed zenity"
 
-if [[ -d ~/.icons ]]
-then
-    cp -r webappmanager.png ~/.icons
-else
-    mkdir ~/.icons
-    cp -r webappmanager.png ~/.icons
-fi
+sudo cp -r webappmanager.png /usr/share/icons/
 sudo cp -r webappmanager /usr/bin/
 sudo chmod +x /usr/bin/webappmanager
-cd ~/.local/share/applications/
-echo "[Desktop Entry]
+cd /usr/share/applications/
+sudo echo "[Desktop Entry]
 Type=Application
 Encoding=UTF-8
 Name=Web App Manager
@@ -24,5 +18,5 @@ Comment=A tool for creating Web Apps
 Exec=webappmanager
 Icon=webappmanager
 Terminal=false" >> "webappmanager.desktop"
-chmod +x webappmanager.desktop
+sudo chmod +x webappmanager.desktop
 zenity --info --title "Installer" --text "Successfully installed WebApp Manager" --no-wrap
